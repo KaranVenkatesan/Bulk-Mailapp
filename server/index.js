89b617dd1956d2ec1b41808f86384bc2c25be13e
KaranVenkatesan <vkaranraina@gmail.com>
2025-03-11 20:14:02 +0530

    npm updated

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 
-const allowedOrigins = ['https://bulk-mailapp-client.vercel.app'];
+const allowedOrigins = ['https://bulk-mailapp-client.vercel.app/'];
 const app = express()
 
 
